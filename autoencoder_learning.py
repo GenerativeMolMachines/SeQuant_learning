0190534a1c0efa3d2ae1f78a4b3bd9501368e94f
Evgeniy Nam <ew.nam@yandex.ru>
2024-04-15 19:13:05 +0300

    fixing paths for saving train history

--- autoencoder_learning.py
+++ autoencoder_learning.py
@@ -174,7 +174,7 @@ history = autoencoder.fit(
     callbacks=[early_stop, model_checkpoint_callback]
 )
 
-with open('trainHistoryDict' + str(num_seq) + '_maxlen' + str(max_len) + '_' + str(
+with open('trainHistoryDict/trainHistoryDict' + str(num_seq) + '_maxlen' + str(max_len) + '_' + str(
         pad) + 'pad_alldescs_norm-1to1_batch' + str(batch_size) + '_lr' + str(learning_rate), 'wb') as file_pi:
     pickle.dump(history.history, file_pi)
 
