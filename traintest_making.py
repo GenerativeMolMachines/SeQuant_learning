0142527b5e49b12bd49cf56a31b9a263efb282a5
Evgenia Din <djzhendogg.is.busy2@gmail.com>
2024-04-23 14:59:48 +0300

    traintest_making.py

--- traintest_making.py
+++ traintest_making.py
@@ -19,39 +19,38 @@ labeled_data = labeled_data.fillna(0)
 labeled_data = labeled_data.drop(labeled_data[labeled_data.SEQ.str.contains(r'[@#&$%+-/*BXZ]')].index)
 labeled_data_seqs = labeled_data['SEQ'].to_list()
 
-# # Processing unlabeled data (Non-AMPs and CPPBase)
-# with open('../data/Non-AMPs.txt') as f:
-#     file = f.readlines()
-# raw_seqs = file[1::2]
-# unlabeled_data = [s.replace("\n", "") for s in raw_seqs]
-#
-# with open('../data/natural_pep (cpp).txt') as f:
-#     file = f.readlines()
-# raw_seqs = file[1::2]
-# unlabeled_data_2 = [s.replace("\n", "") for s in raw_seqs]
-#
-# with open('../data/uniprot_sprot.txt') as f:
-#     raw_seqs_2 = f.readlines()
-# unlabeled_data_3 = [s.replace("\n", "") for s in raw_seqs_2]
-#
-# with open('../data/SPENCER_ORF_protein_sequence.txt') as f:
-#     raw_seqs_2 = f.readlines()
-# unlabeled_data_4 = [s.replace("\n", "") for s in raw_seqs_2]
-#
-# with open('../data/hspvfullR58HET.txt') as f:
-#     raw_seqs_2 = f.readlines()
-# unlabeled_data_5 = [s.replace("\n", "") for s in raw_seqs_2]
+# Processing unlabeled data (Non-AMPs and CPPBase)
+with open('../data/Non-AMPs.txt') as f:
+    file = f.readlines()
+raw_seqs = file[1::2]
+unlabeled_data = [s.replace("\n", "") for s in raw_seqs]
+
+with open('../data/natural_pep (cpp).txt') as f:
+    file = f.readlines()
+raw_seqs = file[1::2]
+unlabeled_data_2 = [s.replace("\n", "") for s in raw_seqs]
+
+with open('../data/uniprot_sprot.txt') as f:
+    raw_seqs_2 = f.readlines()
+unlabeled_data_3 = [s.replace("\n", "") for s in raw_seqs_2]
+
+with open('../data/SPENCER_ORF_protein_sequence.txt') as f:
+    raw_seqs_2 = f.readlines()
+unlabeled_data_4 = [s.replace("\n", "") for s in raw_seqs_2]
+
+with open('../data/hspvfullR58HET.txt') as f:
+    raw_seqs_2 = f.readlines()
+unlabeled_data_5 = [s.replace("\n", "") for s in raw_seqs_2]
 
 # DBAASP database
-# dbaasp = pd.read_csv('../data/peptides.csv')
-# dbaasp_2 = pd.read_csv('../data/peptides_2.csv')
-# unlabeled_data_6 = list(
-#     dict.fromkeys(dbaasp['SEQUENCE'].astype('str').tolist() + dbaasp_2['SEQUENCE'].astype('str').tolist()))
+dbaasp = pd.read_csv('../data/peptides.csv')
+dbaasp_2 = pd.read_csv('../data/peptides_2.csv')
+unlabeled_data_6 = list(
+    dict.fromkeys(dbaasp['SEQUENCE'].astype('str').tolist() + dbaasp_2['SEQUENCE'].astype('str').tolist()))
 
 
 # Merged data for CAE training
-# all_seqs = labeled_data_seqs + unlabeled_data + unlabeled_data_2 + unlabeled_data_3 + unlabeled_data_4 + unlabeled_data_5 + unlabeled_data_6
-all_seqs = labeled_data_seqs
+all_seqs = labeled_data_seqs + unlabeled_data + unlabeled_data_2 + unlabeled_data_3 + unlabeled_data_4 + unlabeled_data_5 + unlabeled_data_6
 all_seqs = [seq for seq in all_seqs if len(seq) <= n]
 all_seqs = list(dict.fromkeys(all_seqs))
 all_seqs = [string.upper() for string in all_seqs]
@@ -66,12 +65,12 @@ df = pd.DataFrame()
 df['s'] = all_seqs_full
 del all_seqs_full
 del labeled_data_seqs
-# del unlabeled_data
-# del unlabeled_data_2
-# del unlabeled_data_3
-# del unlabeled_data_4
-# del unlabeled_data_5
-# del unlabeled_data_6
+del unlabeled_data
+del unlabeled_data_2
+del unlabeled_data_3
+del unlabeled_data_4
+del unlabeled_data_5
+del unlabeled_data_6
 
 df['len']  = df['s'].str.len()
 for i in aa_dict.keys():
