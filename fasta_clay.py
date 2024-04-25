a0de15089b020130401805550d8202c4854b40d9
djzhendogg <djzhendogg.is.busy2@gmail.com>
2024-04-25 21:46:09 +0300

    pkl file maker6

--- fasta_clay.py
+++ fasta_clay.py
@@ -17,19 +17,18 @@ def make_pickle_files(
         one_len_seq_list.append(sequence)
 
     one_len_seq_list_unique = list(set(one_len_seq_list))
+    print(sequence_files_name, len(one_len_seq_list_unique))
     list_seq.extend(one_len_seq_list_unique)
 
-    with open("seq_40_96.pkl", 'wb') as f:
-        pickle.dump(list_seq, f)
-    del one_len_seq_list_unique
 
-
-if __name__ == "__main__":
-    files_names_40_96 = [f"data/sequence_{num}.fasta" for num in range(40, 97, 1)]
-    func_out = Parallel(n_jobs=-1)(
-        [
-            delayed(make_pickle_files)(
-                file_name
-            ) for file_name in files_names_40_96
-        ]
-    )
+files_names_40_96 = [f"data/sequence_{num}.fasta" for num in range(40, 97, 1)]
+func_out = Parallel(n_jobs=-1)(
+    [
+        delayed(make_pickle_files)(
+            file_name
+        ) for file_name in files_names_40_96
+    ]
+)
+print(len(list_seq))
+with open("seq_40_96.pkl", 'wb') as f:
+    pickle.dump(list_seq, f)
