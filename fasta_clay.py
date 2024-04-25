14315957eea9d2a9d65b1c8df10eb81824f884a4
djzhendogg <djzhendogg.is.busy2@gmail.com>
2024-04-25 22:48:17 +0300

    update parser

--- fasta_clay.py
+++ fasta_clay.py
@@ -4,30 +4,38 @@ import pickle
 
 
 def make_pickle_files(
-    sequence_files_name: list
+    sequence_files_name: list,
+    ready_list: list,
+    known_aa: set
 ):
-    list_seq = []
     for file_name in sequence_files_name:
         one_len_seq_list = []
         fasta_sequences = SeqIO.parse(open(file_name), 'fasta')
 
         for fasta in fasta_sequences:
             sequence = str(fasta.seq)
-            one_len_seq_list.append(sequence)
+            if sequence not in ready_list and set(sequence).issubset(known_aa):
+                one_len_seq_list.append(sequence)
 
         one_len_seq_list_unique = list(set(one_len_seq_list))
         print(file_name, len(one_len_seq_list_unique))
-        list_seq.extend(one_len_seq_list_unique)
-        print(len(list_seq))
+        ready_list.extend(one_len_seq_list_unique)
+        print(len(ready_list))
 
-    print(len(list_seq))
-    with open("seq_40_96.pkl", 'wb') as f:
-        pickle.dump(list_seq, f)
+    with open("seq_40_96_with_bact.pkl", 'wb') as f:
+        pickle.dump(ready_list, f)
 
 
 if __name__ == "__main__":
+    with open(r"data/seq_40_96.pkl", "rb") as input_file:
+        already_exist = pickle.load(input_file)
+    aa_set = {'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
+              'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y'}
+
+    clean_ready = [seq for seq in already_exist if set(seq).issubset(aa_set)]
+    print('clean_ready', len(clean_ready))
     files_names_40_96 = [f"data/sequence_{num}.fasta" for num in range(40, 97, 1)]
-    make_pickle_files(files_names_40_96)
+    make_pickle_files(files_names_40_96, clean_ready, aa_set)
 # func_out = Parallel(n_jobs=-1)(
 #     [
 #         delayed(make_pickle_files)(
--- parser.py
+++ parser.py
@@ -1,51 +1,94 @@
-import pandas as pd
 import pickle
-from Bio import Entrez, SeqIO
+import requests
 
-monomer_dict = {
-    'A': 'CC(C(=O)O)N', 'R': 'C(CC(C(=O)O)N)CN=C(N)N', 'N': 'C(C(C(=O)O)N)C(=O)N',
-    'D': 'C(C(C(=O)O)N)C(=O)O', 'C': 'C(C(C(=O)O)N)S', 'Q': 'C(CC(=O)N)C(C(=O)O)N',
-    'E': 'C(CC(=O)O)C(C(=O)O)N', 'G': 'C(C(=O)O)N', 'H': 'C1=C(NC=N1)CC(C(=O)O)N',
-    'I': 'CCC(C)C(C(=O)O)N', 'L': 'CC(C)CC(C(=O)O)N', 'K': 'C(CCN)CC(C(=O)O)N',
-    'M': 'CSCCC(C(=O)O)N', 'F': 'C1=CC=C(C=C1)CC(C(=O)O)N', 'P': 'C1CC(NC1)C(=O)O',
-    'S': 'C(C(C(=O)O)N)O', 'T': 'CC(C(C(=O)O)N)O', 'W': 'C1=CC=C2C(=C1)C(=CN2)CC(C(=O)O)N',
-    'Y': 'C1=CC(=CC=C1CC(C(=O)O)N)O', 'V': 'CC(C)C(C(=O)O)N', 'O': 'CC1CC=NC1C(=O)NCCCCC(C(=O)O)N',
-    'U': 'C(C(C(=O)O)N)[Se]'
+
+species = "Homo sapiens"
+length = 40
+base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
+search_url = base_url + "esearch.fcgi"
+fetch_url = base_url + "efetch.fcgi"
+aa_set = {'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
+              'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y'}
+# Step 1: Perform the search to get the IDs of matching sequences
+search_params = {
+    "db": "protein",
+    "term": f"({length}[SLEN]",
+    "retmax": 600000,  # Adjust retmax as needed
+    "retmode": "json"
 }
+response = requests.get(search_url, params=search_params)
+search_results = response.json()
+
+if int(search_results["esearchresult"]["count"]) == 0:
+    print("No sequences found with the specified length.")
+
+id_list = search_results["esearchresult"]["idlist"]
+
+# Step 2: Fetch the sequences using the IDs
+fetch_params = {
+    "db": "protein",
+    "id": ",".join(id_list),
+    "rettype": "fasta",
+    "retmode": "text"
+}
+response = requests.get(fetch_url, params=fetch_params)
+
+if response.ok:
+    sequences_text = response.text.split('\n\n')[:-1]
+    seq_list = []
+    for b in sequences_text:
+        c = b.split('\n')
+        s = c[1]
+        if s not in seq_list and set(s).issubset(aa_set):
+            seq_list.append(c[1])
+
+    print(seq_list[0])
+    print('len(seq_list)', len(seq_list))
+    with open(f"seq_{length}.pkl", 'wb') as f:
+        pickle.dump(seq_list, f)
+else:
+    print(f"Failed to fetch sequences: {response.status_code} - {response.reason}")
 
 
-def taking_id_len_sequence(name):
-    Entrez.email = "susanjyaks@gmail.com" # Always tell NCBI who you are
-    handle = Entrez.esearch(db="protein", term=name, retmax="1")
-    record = Entrez.read(handle)
-    id_num = record["IdList"][0]
-    handle = Entrez.efetch(db="protein", id=id_num, rettype="fasta", retmode="text")
-    res = SeqIO.read(handle, 'fasta')
-    seq = str(res.seq)
-    lengh = len(seq)
-    return id_num, lengh, seq
 
 
-big_seq_df = pd.read_pickle('data/ncbi_gen_clusters.pkl')
 
-X = pd.read_csv('data/PCLA_proteins.txt', sep="\t", header=0)
-a = X[X['length'] < 97]
-plc_df = a[a['length'] > 40]
-df = pd.concat([plc_df, big_seq_df])
 
-prot_list = df['accession'].unique()
 
-full_s = []
-for name in prot_list:
-    try:
-        _, s_len, sequence = taking_id_len_sequence(name=name)
-        if set(sequence).issubset(set(monomer_dict)):
-            if sequence not in full_s:
-                full_s.append(sequence)
-                print(f"{name} success")
-    except:
-        continue
 
-print('len full_s', len(full_s))
-with open('clusters_prot_40_90.pkl', 'wb') as f:
-    pickle.dump(full_s, f)
+
+# def taking_id_len_sequence(name):
+#     Entrez.email = "susanjyaks@gmail.com" # Always tell NCBI who you are
+#     handle = Entrez.esearch(db="protein", term=name, retmax="1")
+#     record = Entrez.read(handle)
+#     id_num = record["IdList"][0]
+#     handle = Entrez.efetch(db="protein", id=id_num, rettype="fasta", retmode="text")
+#     res = SeqIO.read(handle, 'fasta')
+#     seq = str(res.seq)
+#     lengh = len(seq)
+#     return id_num, lengh, seq
+#
+#
+# big_seq_df = pd.read_pickle('data/ncbi_gen_clusters.pkl')
+#
+# X = pd.read_csv('data/PCLA_proteins.txt', sep="\t", header=0)
+# a = X[X['length'] < 97]
+# plc_df = a[a['length'] > 40]
+# df = pd.concat([plc_df, big_seq_df])
+#
+# prot_list = df['accession'].unique()
+#
+# full_s = []
+# for name in prot_list:
+#     try:
+#         _, s_len, sequence = taking_id_len_sequence(name=name)
+#         if set(sequence).issubset(set(monomer_dict)):
+#             if sequence not in full_s:
+#                 full_s.append(sequence)
+#                 print(f"{name} success")
+#     except:
+#         continue
+#
+# print('len full_s', len(full_s))
+# with open('clusters_prot_40_90.pkl', 'wb') as f:
+#     pickle.dump(full_s, f)
