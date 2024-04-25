20f3ecf16ecf6c589f2a488eb0cc7126a8250f0d
djzhendogg <djzhendogg.is.busy2@gmail.com>
2024-04-25 15:25:43 +0300

    pkl file maker1

--- fasta_clay.py
+++ fasta_clay.py
@@ -1,41 +1,34 @@
 from Bio import SeqIO
 import pickle
 from joblib import Parallel, delayed
-from tqdm import tqdm
 
 
 def make_pickle_files(
-    sequence_files_name: list,
-    min_point,
-    max_point
+    sequence_files_name: list
 ):
     list_seq = []
-    for file_name in tqdm(sequence_files_name):
+    for file_name in sequence_files_name:
+        one_len_seq_list = []
         fasta_sequences = SeqIO.parse(open(file_name), 'fasta')
 
         for fasta in fasta_sequences:
             name, sequence = fasta.id, str(fasta.seq)
-            list_seq.append(sequence)
-    with open(f"seq_{min_point}_{max_point}.pkl", 'wb') as f:
-        pickle.dump(list_seq, f)
+            one_len_seq_list.append(sequence)
+
+        one_len_seq_list_unique = list(set(one_len_seq_list))
+        list_seq.extend(one_len_seq_list_unique)
+
+        with open(f"seq_40_96.pkl", 'wb') as f:
+            pickle.dump(list_seq, f)
+        del one_len_seq_list_unique
 
 
 if __name__ == "__main__":
-    list_s = []
-    start_points = [40, 61, 81]
-    end_points = [60, 80, 96]
-    files_names_40_60 = [f"data/sequence_{num}.fasta" for num in range(40, 61, 1)]
-    files_names_61_80 = [f"data/sequence_{num}.fasta" for num in range(61, 81, 1)]
-    files_names_81_96 = [f"data/sequence_{num}.fasta" for num in range(81, 97, 1)]
-    all_file_names = [files_names_40_60, files_names_61_80, files_names_81_96]
+    files_names_40_96 = [f"data/sequence_{num}.fasta" for num in range(40, 97, 1)]
     func_out = Parallel(n_jobs=-1)(
         [
             delayed(make_pickle_files)(
-                file_name, start_point, end_point
-            ) for file_name, start_point, end_point in zip(
-                all_file_names,
-                start_points,
-                end_points
-            )
+                file_name
+            ) for file_name in files_names_40_96
         ]
     )
