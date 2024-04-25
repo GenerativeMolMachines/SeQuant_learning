dfb45d9378f289723798cd1b06b26fd45731b080
djzhendogg <djzhendogg.is.busy2@gmail.com>
2024-04-25 21:55:16 +0300

    pkl file maker7

--- fasta_clay.py
+++ fasta_clay.py
@@ -1,34 +1,40 @@
 from Bio import SeqIO
 import pickle
-from joblib import Parallel, delayed
-
-
-list_seq = []
+# from joblib import Parallel, delayed
 
 
 def make_pickle_files(
-    sequence_files_name: str
+    sequence_files_name: list
 ):
-    one_len_seq_list = []
-    fasta_sequences = SeqIO.parse(open(sequence_files_name), 'fasta')
+    list_seq = []
+    for file_name in sequence_files_name:
+        one_len_seq_list = []
+        fasta_sequences = SeqIO.parse(open(file_name), 'fasta')
 
-    for fasta in fasta_sequences:
-        sequence = str(fasta.seq)
-        one_len_seq_list.append(sequence)
+        for fasta in fasta_sequences:
+            sequence = str(fasta.seq)
+            one_len_seq_list.append(sequence)
 
-    one_len_seq_list_unique = list(set(one_len_seq_list))
-    print(sequence_files_name, len(one_len_seq_list_unique))
-    list_seq.extend(one_len_seq_list_unique)
+        one_len_seq_list_unique = list(set(one_len_seq_list))
+        print(sequence_files_name, len(one_len_seq_list_unique))
+        list_seq.extend(one_len_seq_list_unique)
+        print(len(list_seq))
+
+    print(len(list_seq))
+    with open("seq_40_96.pkl", 'wb') as f:
+        pickle.dump(list_seq, f)
 
 
-files_names_40_96 = [f"data/sequence_{num}.fasta" for num in range(40, 97, 1)]
-func_out = Parallel(n_jobs=-1)(
-    [
-        delayed(make_pickle_files)(
-            file_name
-        ) for file_name in files_names_40_96
-    ]
-)
-print(len(list_seq))
-with open("seq_40_96.pkl", 'wb') as f:
-    pickle.dump(list_seq, f)
+if __name__ == "__main__":
+    files_names_40_96 = [f"data/sequence_{num}.fasta" for num in range(40, 97, 1)]
+    make_pickle_files(files_names_40_96)
+# func_out = Parallel(n_jobs=-1)(
+#     [
+#         delayed(make_pickle_files)(
+#             file_name
+#         ) for file_name in files_names_40_96
+#     ]
+# )
+#     print(len(list_seq))
+#     with open("seq_40_96.pkl", 'wb') as f:
+#         pickle.dump(list_seq, f)
