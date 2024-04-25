8b25ec6f574fd5ccdec9c1bce6f4c86c84c92d6a
djzhendogg <djzhendogg.is.busy2@gmail.com>
2024-04-26 00:13:11 +0300

    update parser3

--- parser.py
+++ parser.py
@@ -1,60 +1,62 @@
 import pickle
 import requests
 import numpy as np
-from tqdm import tqdm
 
 
-species = "Homo sapiens"
-length = 40
 base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
 search_url = base_url + "esearch.fcgi"
 fetch_url = base_url + "efetch.fcgi"
 aa_set = {'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
               'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y'}
 # Step 1: Perform the search to get the IDs of matching sequences
-search_params = {
-    "db": "protein",
-    "term": f"({length}[SLEN] AND ((animals[filter] OR bacteria[filter]))",
-    "retmax": 200000,  # Adjust retmax as needed
-    "retmode": "json"
-}
-response = requests.get(search_url, params=search_params)
-search_results = response.json()
-
-if int(search_results["esearchresult"]["count"]) == 0:
-    print("No sequences found with the specified length.")
-
-id_list = search_results["esearchresult"]["idlist"]
-
-several_id_lists = np.array_split(np.asarray(search_results["esearchresult"]["idlist"]), 2000)
-seq_list = []
-print('start_parse')
-# Step 2: Fetch the sequences using the IDs
-for id_l in tqdm(several_id_lists):
-    fetch_params = {
+all_sequences = []
+for length in range(40, 97, 1):
+    search_params = {
         "db": "protein",
-        "id": ",".join(list(id_l)),
-        "rettype": "fasta",
-        "retmode": "text"
+        "term": f"({length}[SLEN] AND ((animals[filter] OR bacteria[filter]))",
+        "retmax": 300000,  # Adjust retmax as needed
+        "retmode": "json"
     }
-    response = requests.get(fetch_url, params=fetch_params)
+    response = requests.get(search_url, params=search_params)
+    search_results = response.json()
 
-    if response.ok:
-        print('ok')
-        sequences_text = response.text.split('\n\n')[:-1]
-        for b in sequences_text:
-            c = b.split('\n')
-            s = c[1]
-            if s not in seq_list and set(s).issubset(aa_set):
-                seq_list.append(c[1])
-    else:
-        print(f"Failed to fetch sequences: {response.status_code} - {response.reason}")
+    if int(search_results["esearchresult"]["count"]) == 0:
+        print(f"No sequences found with the specified length={length}.")
+        continue
 
-print(seq_list[0])
-print('len(seq_list)', len(seq_list))
-with open(f"seq_{length}.pkl", 'wb') as f:
-    pickle.dump(seq_list, f)
+    id_list = search_results["esearchresult"]["idlist"]
 
+    several_id_lists = np.array_split(np.asarray(search_results["esearchresult"]["idlist"]), 3000)
+    seq_list = []
+    # Step 2: Fetch the sequences using the IDs
+    for id_l in several_id_lists:
+        if len(seq_list) > 80000:
+            break
+        fetch_params = {
+            "db": "protein",
+            "id": ",".join(list(id_l)),
+            "rettype": "fasta",
+            "retmode": "text"
+        }
+        response = requests.get(fetch_url, params=fetch_params)
+
+        if response.ok:
+            sequences_text = response.text.split('\n\n')[:-1]
+            for b in sequences_text:
+                c = b.split('\n')
+                s = c[1]
+                if s not in seq_list and set(s).issubset(aa_set):
+                    seq_list.append(c[1])
+        else:
+            print(f"Failed to fetch sequences length={length}: {response.status_code} - {response.reason}")
+
+    with open(f"data/pkl_from_parser/seq_{length}_{len(seq_list)}.pkl", 'wb') as f:
+        pickle.dump(seq_list, f)
+
+    all_sequences.extend(seq_list)
+
+with open(f"seq_all_parser_{len(all_sequences)}.pkl", 'wb') as f:
+    pickle.dump(all_sequences, f)
 
 
 
