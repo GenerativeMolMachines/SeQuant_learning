e42be58076cc80dde8029ecbda4c1bee2d770cb7
EvgeniyNam <ew.nam@yandex.ru>
2024-04-26 16:45:46 +0300

    saving 10, 11, 12, 13 arch training

---
+++ learning_logs/learning.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+python3 autoencoder_learning_10.py > log_10.txt
+python3 autoencoder_learning_11.py > log_11.txt
+python3 autoencoder_learning_12.py > log_12.txt
+python3 autoencoder_learning_13.py > log_13.txt
+
