205fea178690ae89946981c91918130a533d38c1
djzhendogg <djzhendogg.is.busy2@gmail.com>
2024-05-01 14:36:04 +0300

    statistics len1

--- len_statistics.py
+++ len_statistics.py
@@ -7,7 +7,8 @@ for folder in ['pkl_from_parser', 'pkl_from_parser_prl']:
     files = os.listdir(check_files_path)
     for file_name in files:
         name_split = file_name.split('_')
-        len_dict[name_split[1]] = int(name_split[2])
+        real_len = name_split[2].split('.')
+        len_dict[name_split[1]] = int(real_len[0])
 
 with open('len_dict_40_96.json', 'w') as fp:
     json.dump(len_dict, fp)
