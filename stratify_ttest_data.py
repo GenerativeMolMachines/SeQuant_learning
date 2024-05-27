96b624cf5d5c361db8849a43428b601f164e630c
Evgenia Din <djzhendogg.is.busy2@gmail.com>
2024-05-27 17:21:46 +0300

    update str

--- stratify_ttest_data.py
+++ stratify_ttest_data.py
@@ -12,9 +12,9 @@ def sts_clay(folders):
         files = os.listdir(check_files_path)
         for file in files:
             full_one_len_list = open_pkl_files(folder, file)
-            n = int(len(full_one_len_list)*0.3)
-            test_one_len = random.sample(full_one_len_list, n)
-            train_one_len = list(set(full_one_len_list) - set(test_one_len))
+            test_one_len = random.sample(full_one_len_list, 360)
+            for_train_one_len = list(set(full_one_len_list) - set(test_one_len))
+            train_one_len = random.sample(for_train_one_len, 840)
             print(f"for len={len(train_one_len[0])} test_len = {len(test_one_len)}, train_len = {len(train_one_len)}")
             test.extend(test_one_len)
             train.extend(train_one_len)
@@ -23,8 +23,8 @@ def sts_clay(folders):
 
 if __name__ == "__main__":
     train_l, test_l = sts_clay(['pkl_from_parser_prl_5_40', 'pkl_from_parser', 'pkl_from_parser_prl'])
-    with open(f"data/train_seq_clean_str.pkl", 'wb') as f:
+    with open(f"data/train_prot_for_masha.pkl", 'wb') as f:
         pickle.dump(train_l, f)
 
-    with open(f"data/test_seq_clean_str.pkl", 'wb') as f:
+    with open(f"data/test_prot_for_masha.pkl", 'wb') as f:
         pickle.dump(test_l, f)
