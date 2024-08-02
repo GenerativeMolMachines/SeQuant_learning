3c4a4ebaca99e258ce8f323f662f37f4bcc8cabb
Evgeniy Nam <ew.nam@yandex.ru>
2024-08-02 16:16:57 +0300

    add code for additional filters try

---
+++ add_filt.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+python3 autoencoder_learning_add_filt.py > log_full_add_filt.txt
+
+
---
+++ autoencoder_filt.py
@@ -0,0 +1,134 @@
+import tensorflow as tf
+from keras.layers import (
+    Input,
+    Dense,
+    Conv2D,
+    AveragePooling2D,
+    BatchNormalization,
+    Flatten,
+    Reshape,
+    Conv2DTranspose,
+    LeakyReLU,
+    Activation,
+    Dropout
+)
+from keras.models import Model
+from keras.engine.keras_tensor import KerasTensor
+
+
+def encoder(x: KerasTensor, height: int) -> tf.Tensor:
+    # First block
+    filters = height * 1
+    x = Conv2D(filters, (1, 4), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = AveragePooling2D((1, 3))(x)
+    x = Dropout(0.1)(x)
+
+    # Second block
+    filters = height * 2
+    x = Conv2D(filters, (1, 4), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = AveragePooling2D((1, 2))(x)
+    x = Dropout(0.1)(x)
+
+    # Third block
+    filters = height * 4
+    x = Conv2D(filters, (1, 4), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = AveragePooling2D((1, 2))(x)
+    x = Dropout(0.1)(x)
+
+    # Fourth block
+    filters = height * 6
+    x = Conv2D(filters, (1, 4), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = AveragePooling2D((1, 2))(x)
+    x = Dropout(0.1)(x)
+
+    # Fifth block
+    filters = height * 8
+    x = Conv2D(filters, (1, 4), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = AveragePooling2D((1, 2))(x)
+    x = Dropout(0.1)(x)
+
+    # Sixth block
+    filters = height * 10
+    x = Conv2D(filters, (1, 4), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = AveragePooling2D((1, 2))(x)
+    x = Dropout(0.1)(x)
+    return x
+
+
+def latent_space(x: KerasTensor, latent_dim: int) -> tf.Tensor:
+    x = Flatten()(x)
+    units = x.shape[1]
+    x = Dense(units)(x)
+    x = Dense(latent_dim, name='Latent')(x)
+    x = Dense(units)(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = Reshape((latent_dim, 1, 1))(x)
+    return x
+
+
+def decoder(x: KerasTensor, height: int) -> tf.Tensor:
+    # First block
+    filters = height * 10
+    x = Conv2DTranspose(filters, (1, 4), strides=(1, 2), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+
+    # Second block
+    filters = height * 8
+    x = Conv2DTranspose(filters, (1, 4), strides=(1, 2), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+
+    # Third block
+    filters = height * 6
+    x = Conv2DTranspose(filters, (1, 4), strides=(1, 2), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+
+    # Fourth block
+    filters = height * 4
+    x = Conv2DTranspose(filters, (1, 4), strides=(1, 2), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+
+    # Fifth block
+    filters = height * 2
+    x = Conv2DTranspose(filters, (1, 4), strides=(1, 2), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.2)(x)
+
+    # Sixth block
+    filters = height * 1
+    x = Conv2DTranspose(filters, (1, 4), strides=(1, 3), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation('tanh')(x)
+    return x
+
+
+def autoencoder_model(height: int, width: int, channels: int, latent_dim: int, learning_rate: float) -> tf.keras.Model:
+    gpus = tf.config.list_logical_devices('GPU')
+    strategy = tf.distribute.MirroredStrategy(gpus)
+    with strategy.scope():
+        inputs = Input(shape=(height, width, channels))
+        x = inputs
+        x = encoder(x, height)
+        x = latent_space(x, latent_dim)
+        output = decoder(x, height)
+        autoencoder = Model(inputs, output, name='Conv_Autoencoder')
+        autoencoder.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+            loss='mean_squared_error'
+        )
+    return autoencoder
