55a07aa9a259f6e033fc6e2dad517ef8564cf3a0
Evgeniy Nam <ew.nam@yandex.ru>
2024-10-22 18:53:36 +0300

    add embeddings saving for each filter strategy

--- autoencoder_learning_07.py
+++ autoencoder_learning_07.py
@@ -32,7 +32,7 @@ channels = 1
 latent_dim = height
 learning_rate = 1e-3
 batch_size = 32
-epochs = 100
+epochs = 50
 tf.keras.backend.clear_session()
 tf.random.set_seed(2022)
 os.environ["KERAS_BACKEND"] = "tensorflow"
@@ -59,8 +59,17 @@ small_test_batches = [small_test_data[i:i + batch_size] for i in range(0, len(sm
 small_train_dataset = create_dataset_from_batches(batches=small_train_batches, monomer_dict=monomer_dict, max_len=max_len)
 small_test_dataset = create_dataset_from_batches(batches=small_test_batches, monomer_dict=monomer_dict, max_len=max_len)
 
+# Filter strategies
 filter_strategy = ['exponential', 'linear', 'height', 'inverse_exponential', 'inverse_linear', 'inverse_height']
 
+# Preparation for embeddings saving
+embeddings_dir = 'embeddings'
+os.makedirs(embeddings_dir, exist_ok=True)
+
+sampled_dataset = small_train_dataset.take(1000)
+sampled_data = np.concatenate([x.numpy() for x, _ in sampled_dataset], axis=0)
+
+# Architectures learning
 for strategy in filter_strategy:
     # Clear previous model info
     tf.keras.backend.clear_session()
@@ -109,5 +118,14 @@ for strategy in filter_strategy:
 
     print("--- %s seconds ---" % (time.time() - start_time))
     print()
+
+    # Embeddings calculation
+    latent_layer_name = 'Latent'
+    latent_model = tf.keras.Model(inputs=autoencoder.input, outputs=autoencoder.get_layer(latent_layer_name).output)
+
+    embeddings = latent_model.predict(sampled_data)
+
+    embeddings_filepath = os.path.join(embeddings_dir, f'embeddings_{strategy}_1000.csv')
+    pd.DataFrame(embeddings).to_csv(embeddings_filepath, index=False)
 
 print('Learning has been finished')
