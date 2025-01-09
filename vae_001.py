cf9102c009dd5f39b7523bfba4f7fa715962266b
Evgeniy Nam <ew.nam@yandex.ru>
2025-01-09 17:23:35 +0300

    additional mean loss printing

--- vae_001.py
+++ vae_001.py
@@ -106,7 +106,9 @@ def vae_loss(original_inputs, outputs, mu, log_var):
     reconstruction_loss = tf.keras.losses.mean_squared_error(original_inputs, outputs)
     kl_loss = -0.5 * tf.reduce_sum(1 + log_var - tf.square(mu) - tf.exp(log_var), axis=-1)
     print(f'Reconstruction loss: {reconstruction_loss}')
+    print(f'Mean reconstruction loss: {tf.reduce_mean(reconstruction_loss)}')
     print(f'KL loss: {kl_loss}')
+    print(f'Mean KL loss: {tf.reduce_mean(kl_loss)}')
     print(tf.reduce_mean(reconstruction_loss + kl_loss))
     return tf.reduce_mean(reconstruction_loss + kl_loss)
 
