abe16fadec63c228ae05d06bb71b9b2b4ed96d6a
Evgenia Din <djzhendogg.is.busy2@gmail.com>
2025-02-12 18:38:26 +0300

    rebuild

--- vae.py
+++ vae.py
@@ -3,10 +3,7 @@ import os
 import keras
 from keras import ops
 from keras import layers
-from keras.layers import (
-    AveragePooling2D,
-    BatchNormalization
-)
+
 os.environ["KERAS_BACKEND"] = "tensorflow"
 
 
@@ -24,11 +21,10 @@ class Sampling(layers.Layer):
 
 
 class VAE(keras.Model):
-    def __init__(self, **kwargs):
+    def __init__(self, encoder, decoder, **kwargs):
         super().__init__(**kwargs)
-        self.latent_dim = 2
-        self.encoder = self.make_encoder(self.latent_dim)
-        self.decoder = self.make_decoder(self.latent_dim)
+        self.encoder = encoder
+        self.decoder = decoder
         self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
         self.reconstruction_loss_tracker = keras.metrics.Mean(
             name="reconstruction_loss"
@@ -67,34 +63,34 @@ class VAE(keras.Model):
             "kl_loss": self.kl_loss_tracker.result(),
         }
 
-    @staticmethod
-    def make_encoder(latent_dim: int):
-        encoder_inputs = keras.Input(shape=(46, 96, 1))
-        x = layers.Conv2D(16, 3, activation="leaky_relu", strides=1, padding="same")(encoder_inputs)
-        x = AveragePooling2D((1, 2))(x)
-        x = layers.Conv2D(32, 2, activation="leaky_relu", strides=1, padding="same")(x)
-        x = AveragePooling2D((1, 2))(x)
-        x = layers.Conv2D(64, 3, activation="leaky_relu", strides=2, padding="same")(x)
-        x = AveragePooling2D((1, 4))(x)
-
-        x = layers.Flatten()(x)
-        x = BatchNormalization()(x)
-        x = layers.Dense(46, name="Latent")(x)
-
-        z_mean = layers.Dense(latent_dim, name="z_mean")(x)
-        z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
-        z = Sampling()([z_mean, z_log_var])
-        encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
-        return encoder
-
-    @staticmethod
-    def make_decoder(latent_dim: int):
-        latent_inputs = keras.Input(shape=(latent_dim,))
-        x = layers.Dense(4416, activation="relu")(latent_inputs)
-        x = layers.Reshape((23, 3, 64))(x)
-        x = layers.Conv2DTranspose(64, 3, activation="leaky_relu", strides=2, padding="same")(x)
-        x = layers.Conv2DTranspose(32, 3, activation="leaky_relu", strides=(1, 4), padding="same")(x)
-        x = layers.Conv2DTranspose(16, 3, activation="leaky_relu", strides=(1, 4), padding="same")(x)
-        decoder_outputs = layers.Conv2DTranspose(1, 3, activation="sigmoid", padding="same")(x)
-        decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
-        return decoder
+    # @staticmethod
+    # def make_encoder(latent_dim: int):
+    #     encoder_inputs = keras.Input(shape=(46, 96, 1))
+    #     x = layers.Conv2D(16, 3, activation="leaky_relu", strides=1, padding="same")(encoder_inputs)
+    #     x = AveragePooling2D((1, 2))(x)
+    #     x = layers.Conv2D(32, 2, activation="leaky_relu", strides=1, padding="same")(x)
+    #     x = AveragePooling2D((1, 2))(x)
+    #     x = layers.Conv2D(64, 3, activation="leaky_relu", strides=2, padding="same")(x)
+    #     x = AveragePooling2D((1, 4))(x)
+    #
+    #     x = layers.Flatten()(x)
+    #     x = BatchNormalization()(x)
+    #     x = layers.Dense(46, name="Latent")(x)
+    #
+    #     z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+    #     z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+    #     z = Sampling()([z_mean, z_log_var])
+    #     encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+    #     return encoder
+    #
+    # @staticmethod
+    # def make_decoder(latent_dim: int):
+    #     latent_inputs = keras.Input(shape=(latent_dim,))
+    #     x = layers.Dense(4416, activation="relu")(latent_inputs)
+    #     x = layers.Reshape((23, 3, 64))(x)
+    #     x = layers.Conv2DTranspose(64, 3, activation="leaky_relu", strides=2, padding="same")(x)
+    #     x = layers.Conv2DTranspose(32, 3, activation="leaky_relu", strides=(1, 4), padding="same")(x)
+    #     x = layers.Conv2DTranspose(16, 3, activation="leaky_relu", strides=(1, 4), padding="same")(x)
+    #     decoder_outputs = layers.Conv2DTranspose(1, 3, activation="sigmoid", padding="same")(x)
+    #     decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+    #     return decoder
--- vae_learning.py
+++ vae_learning.py
@@ -7,10 +7,14 @@ import pandas as pd
 
 import keras
 import tensorflow as tf
-
+from keras import layers
+from keras.layers import (
+    AveragePooling2D,
+    BatchNormalization
+)
 
 from autoencoder_preset_tools import data_processing
-from vae import VAE
+from vae import Sampling, VAE
 
 os.environ["KERAS_BACKEND"] = "tensorflow"
 
@@ -33,6 +37,7 @@ train_dataset = data_processing(batch_data=train_data, max_len=96)
 # test_dataset = data_processing(batch_data=test_data, max_len=96)
 
 gpus = tf.config.list_logical_devices('GPU')
+print(gpus)
 strategy = tf.distribute.MirroredStrategy(gpus)
 with strategy.scope():
     checkpoint_filepath = f'checkpoint/checkpoint_vae_zh' + '.keras'
@@ -46,7 +51,36 @@ with strategy.scope():
 
     early_stop = tf.keras.callbacks.EarlyStopping(monitor='total_loss', patience=3)
 
-    vae = VAE()
+    latent_dim = 2
+    encoder_inputs = keras.Input(shape=(46, 96, 1))
+    x = layers.Conv2D(16, 3, activation="leaky_relu", strides=1, padding="same")(encoder_inputs)
+    x = AveragePooling2D((1, 2))(x)
+    x = layers.Conv2D(32, 2, activation="leaky_relu", strides=1, padding="same")(x)
+    x = AveragePooling2D((1, 2))(x)
+    x = layers.Conv2D(64, 3, activation="leaky_relu", strides=2, padding="same")(x)
+    x = AveragePooling2D((1, 4))(x)
+
+    x = layers.Flatten()(x)
+    x = BatchNormalization()(x)
+    x = layers.Dense(46, name="Latent")(x)
+
+    z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+    z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+    z = Sampling()([z_mean, z_log_var])
+    encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+    print(encoder.summary())
+
+    latent_inputs = keras.Input(shape=(latent_dim,))
+    x = layers.Dense(4416, activation="relu")(latent_inputs)
+    x = layers.Reshape((23, 3, 64))(x)
+    x = layers.Conv2DTranspose(64, 3, activation="leaky_relu", strides=2, padding="same")(x)
+    x = layers.Conv2DTranspose(32, 3, activation="leaky_relu", strides=(1, 4), padding="same")(x)
+    x = layers.Conv2DTranspose(16, 3, activation="leaky_relu", strides=(1, 4), padding="same")(x)
+    decoder_outputs = layers.Conv2DTranspose(1, 3, activation="sigmoid", padding="same")(x)
+    decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+    print(decoder.summary())
+
+    vae = VAE(encoder, decoder)
     vae.compile(optimizer=keras.optimizers.Adam())
     history = vae.fit(train_dataset, epochs=100, batch_size=128, callbacks=[early_stop, model_checkpoint_callback])
     with open(f'trainHistoryDict/vae_01.pkl', 'wb') as file_pi:
